667d300e3593ff10c266ea31ba4fcf72d0321674
DaDvoy <you@example.com>
2021-09-23 19:07:52 +0300

    start ex01

---
+++ module00/ex01/PhoneBook.hpp
@@ -0,0 +1,14 @@
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+#include <iostream>
+#include <string>
+
+class PhoneBook
+{
+private:
+    /* data */
+public:
+    PhoneBook(/* args */);
+    ~PhoneBook();
+};
