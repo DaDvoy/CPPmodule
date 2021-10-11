4fff908dcaeb58768709015fc3f2916e24234c0d
DaDvoy <you@example.com>
2021-10-11 23:33:01 +0300

    start ex02

---
+++ module00/ex02/Account.cpp
@@ -0,0 +1,72 @@
+#include <Account.hpp>
+#include <iostream>
+#include <ctime>
+
+int	_nbAccounts = 0;
+int	_totalAmount = 0;
+int	_totalNbDeposits = 0;
+int	_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) {
+	_accountIndex = _nbAccounts++;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_totalAmount += initial_deposit;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "created" << std::endl;
+}
+
+Account::~Account() {
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "closed" << std::endl;
+}
+
+int		Account::getNbAccounts( void ) {
+	return (_nbAccounts);
+}
+
+int		Account::getTotalAmount( void ) {
+	return (_totalAmount);
+}
+
+int		Account::getNbDeposits( void ) {
+	return (_totalNbDeposits);
+}
+
+int		Account::getNbWithdrawals( void ) {
+	return (_totalNbWithdrawals);
+}
+
+void	Account::_displayTimestamp(void) {
+	std::time_t t = std::time(0);   // get time now
+	std::tm* now = std::localtime(&t);
+	std::cout << "[" << (now->tm_year + 1900)
+	<< (now->tm_mon + 1) << now->tm_mday
+	<< "_" << now->tm_hour << now->tm_min
+	<< now->tm_sec << "]" << "\n";
+}
+
+void	Account::displayAccountsInfos() {
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << ";" << std::endl;
+}
+
+void	Account::displayStatus() const {
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "deposits:" << _nbDeposits << ";";
+	std::cout << "withdrawals:" << _nbWithdrawals << ";" << std::endl;
+}
+
+void    Account::makeDeposit(int deposit) {
+
+}
